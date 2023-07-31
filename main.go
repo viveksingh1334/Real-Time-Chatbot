39ddab1e5f950e64f67f799e41dc3ac8b2aa1888
Vivek Pratap Singh <43885096+viveksingh1334@users.noreply.github.com>
2023-07-31 08:42:10 +0530

    Adding user authentication module

--- main.go
+++ main.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/websocket"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var clients = make(map[*websocket.Conn]bool)
 var broadcast = make(chan Message)
 var upgrader = websocket.Upgrader{}
+
+var messageHistory = []Message{}
+var maxMessageHistory = 100
+
+var secretKey = []byte("your-secret-key")
 
 // Define a Message struct to represent chat messages
 type Message struct {
@@ -18,8 +26,15 @@ type Message struct {
 	Content  string `json:"content"`
 }
 
+// Define a User struct to store user information
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func main() {
 	http.HandleFunc("/ws", handleConnections)
+	http.HandleFunc("/login", loginHandler)
 	go handleMessages()
 
 	fmt.Println("Server is running on http://localhost:8080")
@@ -27,6 +42,13 @@ func main() {
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// Perform user authentication using JWT
+	claims, err := validateToken(r)
+	if err != nil {
+		http.Error(w, "Authentication failed", http.StatusUnauthorized)
+		return
+	}
+
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("Error upgrading connection:", err)
@@ -36,6 +58,14 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 
 	clients[conn] = true
 
+	// Send message history to the new user
+	for _, msg := range messageHistory {
+		conn.WriteJSON(msg)
+	}
+
+	// Notify other users about the new user
+	broadcast <- Message{Username: "Server", Content: claims.Username + " joined the chat"}
+
 	for {
 		var msg Message
 		err := conn.ReadJSON(&msg)
@@ -43,6 +73,12 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 			log.Println("Error reading message:", err)
 			delete(clients, conn)
 			break
+		}
+
+		// Add the message to the history
+		messageHistory = append(messageHistory, msg)
+		if len(messageHistory) > maxMessageHistory {
+			messageHistory = messageHistory[len(messageHistory)-maxMessageHistory:]
 		}
 
 		broadcast <- msg
@@ -61,4 +97,65 @@ func handleMessages() {
 			}
 		}
 	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	// Simulate user authentication
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	user := User{Username: username, Password: string(hashedPassword)}
+
+	// Create a new JWT token
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["username"] = user.Username
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix() // Token expires in 24 hours
+
+	// Sign the token with the secret key
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the token as a cookie in the response
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: time.Now().Add(time.Hour * 24),
+		Path:    "/",
+	})
+
+	// Return a success response with the generated token
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Login successful"))
+}
+
+func validateToken(r *http.Request) (*jwt.MapClaims, error) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		return nil, err
+	}
+
+	tokenString := cookie.Value
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Validate the token signing method
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method")
+		}
+		return secretKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, fmt.Errorf("Invalid token")
+	}
+
+	return &claims, nil
 }
